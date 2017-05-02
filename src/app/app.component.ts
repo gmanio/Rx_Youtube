16f5556f67492d2c0b3fbf5364daff29b07be43b
a1100490 <jiman.park@sk.com>
2017-05-02 23:19:59 +0900

    [ TASK ] youtube service modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
 import * as Rx from 'rxjs/Rx';
 
 import { YoutubeService } from "./services/youtube.service";
@@ -8,17 +8,27 @@ import { YoutubeService } from "./services/youtube.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent implements AfterViewInit, AfterContentInit {
+  ngAfterContentInit(): void {
+    Rx.Observable.fromPromise(this.youtube.getVideos())
+      .map((data) => {
+        return data['result']
+      })
+      .subscribe(this.onLoadVideos);
+  }
 
   constructor(private youtube: YoutubeService) {
+    this.printDevLogo()
+  }
+
+  private printDevLogo() {
+    setTimeout(console.log.bind(console, "%cGMAN", "font:8em Arial;color:#EC6521;font-weight:bold"), 0);
+    setTimeout(console.log.bind(console, "%cDevTools@2017", "font:2em sans-serif;color:#333;"), 0);
 
   }
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      Rx.Observable.fromPromise(this.youtube.getVideos())
-        .subscribe(this.onLoadVideos);
-    }, 1000);
+  ngAfterViewInit(): void {
 
   }
 
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,43 +1,39 @@
-import { Injectable } from '@angular/core';
-import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
 import { WindowRefService } from "./window-ref.service";
 
 @Injectable()
 export class YoutubeService {
-  apiKey = 'AIzaSyA4k_7jggyPzjs1Tv90go3eoRyn5War9LQ';
+  static APPKEY = "AIzaSyA4k_7jggyPzjs1Tv90go3eoRyn5War9LQ";
 
   private gapi: any;
   private oClient: any;
   private request;
 
-  constructor(private http: Http,
-              private windowRef: WindowRefService) {
-
+  constructor(private windowRef: WindowRefService) {
     this.windowRef.nativeWindow.gapi_onload = this.onLoadGapi();
-    // this.gapi = this.windowRef.nativeWindow.gapi;
   }
 
   onLoadGapi() {
     this.gapi = this.windowRef.nativeWindow.gapi;
-    this.gapi.load('client', this.onLoadClient.bind(this))
+    this.gapi.load("client", this.onLoadClient.bind(this))
   }
 
   onLoadClient() {
     this.oClient = this.gapi.client;
-    this.oClient.setApiKey(this.apiKey);
+    this.oClient.setApiKey(YoutubeService.APPKEY);
 
-    this.oClient.load('youtube', 'v3', () => {
+    this.oClient.load("youtube", "v3", () => {
       this.request = this.oClient.youtube.search.list;
     });
   }
 
   getVideos() {
     let initOption = {
-      part: 'snippet', //required
-      type: 'video',
+      part: "snippet", //required
+      type: "video",
       q: "donald",
       maxResults: 20,
-      region: 'KR'
+      region: "KR"
     }
 
     return this.request(initOption);
